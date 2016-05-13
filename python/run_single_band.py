aed263fcc963b2587c080dc6bba0fad2e969bb30
Priyanka Seth <priyanka.seth@polytechnique.edu>
2016-05-13 16:25:16 +0200

    single band notebooks

--- python/run_single_band.py
+++ python/run_single_band.py
@@ -2,7 +2,11 @@ from pytriqs.gf.local import *
 from pytriqs.operators import *
 from pytriqs.archive import *
 from pytriqs.applications.impurity_solvers.cthyb import Solver
-from numpy import *
+import numpy as np
+
+import os
+if not os.path.exists('results'):
+    os.makedirs('results')
 
 # Parameters of the model
 t = 1.0
@@ -13,7 +17,7 @@ n_loops = 10
 S = Solver(beta = beta, gf_struct = {'up':[0], 'down':[0]})
 
 # I run for several values of U
-for U in arange(1.0, 13.0):
+for U in np.arange(1.0, 13.0):
 
     # This is a first guess for G
     S.G_iw << SemiCircular(2*t)
@@ -29,13 +33,12 @@ for U in arange(1.0, 13.0):
             g0 << inverse( iOmega_n + U/2.0 - t**2 * g )
 
         # Solve the impurity problem
-        S.solve(h_loc = U * n('up',0) * n('down',0),   # Local Hamiltonian 
+        S.solve(h_int = U * n('up',0) * n('down',0),   # Local Hamiltonian 
             n_cycles  = 10000,                           # Number of QMC cycles
             n_warmup_cycles = 5000,                      # Warmup cycles
             )
     
         # Save iteration in archive
-        A = HDFArchive("single-U%.2f.h5"%U)
-        A['G-%i'%i] = S.G_iw
-        A['Sigma-%i'%i] = S.Sigma_iw
-        del A
+        with HDFArchive("results/single-U%.2f.h5"%U) as A:
+            A['G-%i'%i] = S.G_iw
+            A['Sigma-%i'%i] = S.Sigma_iw
