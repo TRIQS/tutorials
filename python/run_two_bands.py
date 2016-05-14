67609212a51e3d6c51b925add8ef8c5d103468ef
Priyanka Seth <priyanka.seth@polytechnique.edu>
2016-05-14 15:19:27 +0200

    two band cthyb -- updated script, notebooks and added results

--- python/run_two_bands.py
+++ python/run_two_bands.py
@@ -1,27 +1,30 @@
 from pytriqs.gf.local import *
 from pytriqs.operators import *
 from pytriqs.archive import *
-from pytriqs.applications.impurity_solvers.cthyb_segment import Solver
-from numpy import *
+from pytriqs.applications.impurity_solvers.cthyb import Solver
+from itertools import product
+import numpy as np
+
+import os
+if not os.path.exists('results_two_bands'):
+    os.makedirs('results_two_bands')
 
 # Parameters of the model
 t = 1.0
 beta = 10.0
 n_loops = 10
-filling = 'half'
+filling = 'quarter' #'half'
+n_orbitals = 2
 
 # Construct the segment solver
-S = Solver(beta = beta, block_names = ['up-0', 'up-1', 'down-0', 'down-1'])
-
-# We will need these later
-F = S.G0.copy()
-G = S.G0.copy()
-Sigma = S.G0.copy()
+S = Solver(beta = beta, gf_struct = {'up-0':[0], 'up-1':[0], 'down-0':[0], 'down-1':[0]})
 
 for coeff in [0.0, 0.1, 0.2]:
+#for coeff in [0.1]:
 
-    # I run for several values of U
-    for U in arange(1.0, 13.0, 1.0):
+    # Run for several values of U
+    for U in np.arange(1.0, 13.0, 1.0):
+    #for U in [1.0]:
 
         J = coeff * U
 
@@ -31,17 +34,29 @@ for coeff in [0.0, 0.1, 0.2]:
         elif filling == 'quarter':
             mu = -0.81 + (0.6899-1.1099*coeff)*U + (-0.02548+0.02709*coeff-0.1606*coeff**2)*U**2
 
-        # Interaction matrix
-        Umat = zeros([4,4])
-        for i in range(2):
-            for j in range(2):
-                Umat[i][j] = U-3*J if i != j else 0.0
-                Umat[i+2][j+2] = U-3*J if i != j else 0.0
-                Umat[i][j+2] = U if i == j else U-2*J
-                Umat[i+2][j] = U if i == j else U-2*J
+
+        # Set the interacting Kanamori hamiltonian
+        h_int = Operator()
+        for o in range(0,n_orbitals):
+            h_int += U*n('up-%s'%o,0)*n('down-%s'%o,0)
+        
+        for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
+            if o1==o2: continue
+            h_int += (U-2*J)*n('up-%s'%o1,0)*n('down-%s'%o2,0)
+        
+        for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
+            if o2>=o1: continue;
+            h_int += (U-3*J)*n('up-%s'%o1,0)*n('up-%s'%o2,0)
+            h_int += (U-3*J)*n('down-%s'%o1,0)*n('down-%s'%o2,0)
+        
+        for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
+            if o1==o2: continue
+            h_int += -J*c_dag('up-%s'%o1,0)*c_dag('down-%s'%o1,0)*c('up-%s'%o2,0)*c('down-%s'%o2,0)
+            h_int += -J*c_dag('up-%s'%o1,0)*c_dag('down-%s'%o2,0)*c('up-%s'%o2,0)*c('down-%s'%o1,0)
+
 
         # This is a first guess for G
-        S.G0 <<= inverse(iOmega_n + mu - t**2 * SemiCircular(2*t))
+        S.G0_iw << inverse(iOmega_n + mu - t**2 * SemiCircular(2*t))
 
         # DMFT loop with self-consistency
         for i in range(n_loops):
@@ -49,32 +64,22 @@ for coeff in [0.0, 0.1, 0.2]:
             print "\n\nIteration = %i / %i" % (i+1, n_loops)
 
             # Symmetrize the Green's function and use self-consistency
-            if i>0:
-                g = 0.25 * ( G['up-0'] + G['down-0'] + G['up-1'] + G['down-1'] )
-                for name, g0 in S.G0:
+            if i > 0:
+                g = 0.25 * ( S.G_iw['up-0'] + S.G_iw['up-1'] + S.G_iw['down-0'] + S.G_iw['down-1'] )
+                for name, g0 in S.G0_iw:
                     g0 <<= inverse(iOmega_n + mu - t**2 * g)
 
             # Solve the impurity problem
-            S.solve(U = Umat,
+            S.solve(h_int = h_int,
                     n_cycles  = 30000,
                     length_cycle = 100,
-                    n_warmup_cycles = 5000,
-                    improved_estimator = True)
-
-            # Symmetrize F
-            f_tau = 0.25 * ( S.F_tau['up-0'] + S.F_tau['down-0'] + S.F_tau['up-1'] + S.F_tau['down-1'] )
-
-            # Get G and Sigma
-            for name, f in F:
-                f <<= Fourier(f_tau)
-                G[name] <<= S.G0[name] + S.G0[name] * f
-            Sigma = inverse(S.G0) - inverse(G)
+                    n_warmup_cycles = 5000)
 
             # Check density
-            print G.density()
+            for name, g in S.G_iw:
+                print name, ": ",g.density()[0,0].real
 
             # Save iteration in archive
-            A = HDFArchive("results-%s-U%.2f-J%.2f.h5"%(filling,U,J))
-            A['G-%i'%i] = G
-            A['Sigma-%i'%i] = Sigma
-            del A
+            with HDFArchive("results_two_bands/%s-U%.2f-J%.2f.h5"%(filling,U,J)) as A:
+                A['G-%i'%i] = S.G_iw
+                A['Sigma-%i'%i] = S.Sigma_iw
