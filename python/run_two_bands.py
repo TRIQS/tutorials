73ffa8c3d96007e3970646c674fd63b40731d3e3
Priyanka Seth <priyanka.seth@polytechnique.edu>
2016-05-19 15:50:52 +0200

    two orbital -- more minor changes

--- python/run_two_bands.py
+++ python/run_two_bands.py
@@ -13,18 +13,16 @@ if not os.path.exists('results_two_bands'):
 t = 1.0
 beta = 10.0
 n_loops = 10
-filling = 'quarter' #'half'
+filling = 'half' # or 'quarter'
 n_orbitals = 2
 
-# Construct the segment solver
+# Construct the solver
 S = Solver(beta = beta, gf_struct = {'up-0':[0], 'up-1':[0], 'down-0':[0], 'down-1':[0]})
 
 for coeff in [0.0, 0.1, 0.2]:
-#for coeff in [0.1]:
 
     # Run for several values of U
     for U in np.arange(1.0, 13.0, 1.0):
-    #for U in [1.0]:
 
         J = coeff * U
 
@@ -34,26 +32,21 @@ for coeff in [0.0, 0.1, 0.2]:
         elif filling == 'quarter':
             mu = -0.81 + (0.6899-1.1099*coeff)*U + (-0.02548+0.02709*coeff-0.1606*coeff**2)*U**2
 
-
         # Set the interacting Kanamori hamiltonian
         h_int = Operator()
         for o in range(0,n_orbitals):
             h_int += U*n('up-%s'%o,0)*n('down-%s'%o,0)
-        
         for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
             if o1==o2: continue
             h_int += (U-2*J)*n('up-%s'%o1,0)*n('down-%s'%o2,0)
-        
         for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
             if o2>=o1: continue;
             h_int += (U-3*J)*n('up-%s'%o1,0)*n('up-%s'%o2,0)
             h_int += (U-3*J)*n('down-%s'%o1,0)*n('down-%s'%o2,0)
-        
         for o1,o2 in product(range(0,n_orbitals),range(0,n_orbitals)):
             if o1==o2: continue
             h_int += -J*c_dag('up-%s'%o1,0)*c_dag('down-%s'%o1,0)*c('up-%s'%o2,0)*c('down-%s'%o2,0)
             h_int += -J*c_dag('up-%s'%o1,0)*c_dag('down-%s'%o2,0)*c('up-%s'%o2,0)*c('down-%s'%o1,0)
-
 
         # This is a first guess for G
         S.G0_iw << inverse(iOmega_n + mu - t**2 * SemiCircular(2*t))
@@ -67,7 +60,7 @@ for coeff in [0.0, 0.1, 0.2]:
             if i > 0:
                 g = 0.25 * ( S.G_iw['up-0'] + S.G_iw['up-1'] + S.G_iw['down-0'] + S.G_iw['down-1'] )
                 for name, g0 in S.G0_iw:
-                    g0 <<= inverse(iOmega_n + mu - t**2 * g)
+                    g0 << inverse(iOmega_n + mu - t**2 * g)
 
             # Solve the impurity problem
             S.solve(h_int = h_int,
