c955d86e986ae7baac2c863b1f3a17fcdae38935
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-05-24 14:25:32 -0400

    Restructure tutorial files and cleanup

---
+++ C++/gtodelta.cpp
@@ -0,0 +1,34 @@
+#include <triqs/gfs.hpp>
+#include <triqs/mesh.hpp>
+
+using namespace triqs::gfs;
+using namespace triqs::lattice;
+
+int main() {
+
+ double beta = 10, mu = 0;
+ int n_freq = 100, n_pts = 100;
+
+ // Green's function on Matsubara frequencies, 1x1 matrix-valued.
+ auto Delta_iw = gf<imfreq>{{beta, Fermion, n_freq}, {1, 1}};
+ auto Gloc = gf<imfreq>{{beta, Fermion, n_freq}, {1, 1}};
+
+ // Green's function in imaginary time, 1x1 matrix-valued.
+ auto Delta_tau = gf<imtime>{{beta, Fermion, 2 * n_freq + 1}, {1, 1}};
+
+ auto bz = brillouin_zone{bravais_lattice{{{1, 0}, {0, 1}}}};
+ auto bz_mesh = gf_mesh<brzone>{bz, n_pts};
+
+ nda::clef::placeholder<1> k_;
+ nda::clef::placeholder<2> iw_;
+
+ // The actual equations
+ Gloc(iw_) << sum(1 / (iw_ + mu - 2 * (cos(k_[0]) + cos(k_[1]))), k_ = bz_mesh) / bz_mesh.size(); // (3)
+ Delta_iw(iw_) << iw_ + mu - 1 / Gloc(iw_);                                                       // (4)
+ Delta_tau() = fourier(Delta_iw);                                                         	  // (5)
+
+ // Write the hybridization to an HDF5 archive
+ auto file = h5::file("Delta.h5", 'w');
+ h5_write(file, "Delta_tau", Delta_tau);
+ h5_write(file, "Delta_iw", Delta_iw);
+}
---
+++ ModelDMFT/scripts/one_band.py
@@ -0,0 +1,45 @@
+from triqs.gf import *
+from triqs.operators import *
+from h5 import *
+from triqs_cthyb import Solver
+import numpy as np
+
+import os
+if not os.path.exists('data/one_band'):
+    os.makedirs('data/one_band')
+
+# Parameters of the model
+t = 1.0
+beta = 10.0
+n_loops = 10
+
+# Construct the impurity solver
+S = Solver(beta = beta, gf_struct = [('up',[0]), ('down',[0])] )
+
+# I run for several values of U
+for U in np.arange(1.0, 13.0):
+    print('U =', U)
+
+    # This is a first guess for G
+    S.G_iw << SemiCircular(2*t)
+
+    # DMFT loop with self-consistency
+    for i in range(n_loops):
+    
+        print("\n\nIteration = %i / %i" % (i+1, n_loops))
+    
+        # Symmetrize the Green's function and use self-consistency
+        g = 0.5 * ( S.G_iw['up'] + S.G_iw['down'] )
+        for name, g0 in S.G0_iw:
+            g0 << inverse( iOmega_n + U/2.0 - t**2 * g )
+
+        # Solve the impurity problem
+        S.solve(h_int = U * n('up',0) * n('down',0),   # Local Hamiltonian 
+            n_cycles  = 10000,                           # Number of QMC cycles
+            n_warmup_cycles = 5000,                      # Warmup cycles
+            )
+    
+        # Save iteration in archive
+        with HDFArchive("data/one_band/half-U%.2f.h5"%U) as A:
+            A['G-%i'%i] = S.G_iw
+            A['Sigma-%i'%i] = S.Sigma_iw
