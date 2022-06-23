889ecb4fd177778b6d5704b1c9a6cead0e32087b
Nils Wentzell <nwentzell@flatironinstitute.org>
2022-06-23 16:10:01 -0400

    Clang-format source files in c++ subdir

--- C++/array.cpp
+++ C++/array.cpp
@@ -5,34 +5,34 @@ using namespace nda;
 
 int main() {
 
- auto a = matrix<double>(2, 2);                   // Declare a 2x2 matrix of double
- auto b = array<double, 3>(5, 2, 2);              // Declare a 5x2x2 array of double
- auto c = array<double, 2>{{1, 2, 3}, {4, 5, 6}}; // 2x3 array, with initialization
+  auto a = matrix<double>(2, 2);                   // Declare a 2x2 matrix of double
+  auto b = array<double, 3>(5, 2, 2);              // Declare a 5x2x2 array of double
+  auto c = array<double, 2>{{1, 2, 3}, {4, 5, 6}}; // 2x3 array, with initialization
 
- clef::placeholder<0> i_;
- clef::placeholder<1> j_;
- clef::placeholder<2> k_;
+  clef::placeholder<0> i_;
+  clef::placeholder<1> j_;
+  clef::placeholder<2> k_;
 
- // Assign values
- a(i_, j_) << i_ + j_;
- b(i_, j_, k_) << i_ * a(k_, j_);
+  // Assign values
+  a(i_, j_) << i_ + j_;
+  b(i_, j_, k_) << i_ * a(k_, j_);
 
- std::cout << "a = " << a << std::endl; // Printing
+  std::cout << "a = " << a << std::endl; // Printing
 
- matrix<double> i = inverse(a); // Inverse using LAPACK
- double d = determinant(a);     // Determinant using LAPACK
+  matrix<double> i = inverse(a);     // Inverse using LAPACK
+  double d         = determinant(a); // Determinant using LAPACK
 
- auto ac = a;                 // Make a copy (the container is a regular type)
- ac = a * a + 2 * ac;         // Basic operations (uses BLAS for matrix product)
- b(0, range(), range()) = ac; // Assign ac into partial view of b
+  auto ac                = a;              // Make a copy (the container is a regular type)
+  ac                     = a * a + 2 * ac; // Basic operations (uses BLAS for matrix product)
+  b(0, range(), range()) = ac;             // Assign ac into partial view of b
 
- // Writing the array into an hdf5 file.
- auto f = h5::file("a_file.h5", 'w');
- h5_write(f, "a", a);
+  // Writing the array into an hdf5 file.
+  auto f = h5::file("a_file.h5", 'w');
+  h5_write(f, "a", a);
 
- auto m = max_element(abs(b)); // maximum of the absolute value of the array.
+  auto m = max_element(abs(b)); // maximum of the absolute value of the array.
 
- // A more "functional" example: compute the norm sum_{i,j} |A_{ij}|
- auto lambda = [](double r, double x) { return r + std::abs(x); };
- auto norm = fold(lambda, a, 0);
+  // A more "functional" example: compute the norm sum_{i,j} |A_{ij}|
+  auto lambda = [](double r, double x) { return r + std::abs(x); };
+  auto norm   = fold(lambda, a, 0);
 }
